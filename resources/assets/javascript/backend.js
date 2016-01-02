e04fdb0bc9b232e00200b98dc54361dd8fbbfeb7
Dennis Prochko <wolfsoft@mail.ru>
2016-01-02 06:39:38 +0300

    application update

--- resources/assets/javascript/backend.js
+++ resources/assets/javascript/backend.js
@@ -3,13 +3,14 @@ $(function() {
 var autocompleter = new Bloodhound({
 	queryTokenizer: Bloodhound.tokenizers.whitespace,
 	datumTokenizer: Bloodhound.tokenizers.whitespace,
-	remote: '/admin/categories'
+	remote: '/admin/categories.json'
 });
 
 $('.tag-list').tokenfield({
 	typeahead: [null, {
 		source: autocompleter.ttAdapter()
-	}]
+	}],
+	showAutocompleteOnFocus: true
 });
 
 $('.textarea').summernote({
